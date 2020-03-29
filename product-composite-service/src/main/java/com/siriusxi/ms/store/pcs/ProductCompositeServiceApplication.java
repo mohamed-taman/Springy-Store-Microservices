1d89c4ea9d3e4fcc82ef4fd600dd3de3d896f394
Mohamed Taman <mohamed.taman@gmail.com>
2020-03-30 01:19:37 +0200

    Implements Product Composite Service.
    - Implement service API and integration component.
    - Changed spring boot version from 2.3.0M3 to 2.3.0.M2 to avoid message java.lang.UnsupportedOperationException: This feature requires ASM8_EXPERIMENTAL when using JDK 14 record feature.

--- product-composite-service/src/main/java/com/siriusxi/ms/store/pcs/ProductCompositeServiceApplication.java
+++ product-composite-service/src/main/java/com/siriusxi/ms/store/pcs/ProductCompositeServiceApplication.java
@@ -2,9 +2,18 @@ package com.siriusxi.ms.store.pcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("com.siriusxi.ms.store")
 public class ProductCompositeServiceApplication {
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
--- review-service/src/main/java/com/siriusxi/ms/store/revs/ReviewServiceApplication.java
+++ review-service/src/main/java/com/siriusxi/ms/store/revs/ReviewServiceApplication.java
@@ -2,6 +2,7 @@ package com.siriusxi.ms.store.revs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ReviewServiceApplication {
