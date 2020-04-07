3a72cc345f6e4cb6a9b0c23cf0dfc487b9eb75d7
Nick Stolwijk <nick.stolwijk@rigd-loxia.nl>
2020-04-08 00:00:03 +0200

    Simplify maven build.

--- run-em-all.sh
+++ run-em-all.sh
@@ -6,7 +6,7 @@ echo -e "Starting [Springy Store] μServices ....\n\
 ---------------------------------------\n"
 
 function runService(){
-   ./mvnw --quiet spring-boot:run -Dspring-boot.run.jvmArguments="--enable-preview" -f $1
+   ./mvnw --quiet spring-boot:run -Dspring-boot.run.jvmArguments="--enable-preview" -pl $1
 }
 
 for dir in `find *-service -maxdepth 0 -type d`
